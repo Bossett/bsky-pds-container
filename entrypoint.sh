ebe5bc353c5ee68a6ca74ce329e217261660213a
Bossett <bossett@bossett.io>
2024-03-21 09:37:28 +1100

    Handle actor sqlite into litefs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,24 @@
 
 COMMAND=""
 
+process_sqlite_files() {
+  while true; do
+    sleep 300
+    find "$PDS_ACTOR_STORE_DIRECTORY" -type f -name "*.sqlite" | while read original_path; do
+      processed_path=$(echo $original_path | sed "s|$PDS_ACTOR_STORE_DIRECTORY/||; s|/|_|g; s|:|_|g")
+      full_new_path=$PDS_DATA_DIRECTORY/$processed_path
+      if [ -L "$original_path" ]; then
+        continue
+      fi
+      litefs import -name $processed_path $original_path
+      if [ $? -eq 0 ]; then
+        [ ! -L "$original_path" ] && rm $original_path
+        ln -s $full_new_path $original_path
+      fi
+    done
+  done
+}
+
 if test -f "/pds/litefs.yml"; then
   rm -rf /etc/litefs.yml
   cp /pds/litefs.yml /etc/litefs.yml
@@ -12,6 +30,7 @@ if test -f "/pds/litefs.yml"; then
 
   COMMAND="litefs run -- $COMMAND"
   litefs mount &
+  process_sqlite_files &
 fi
 
 if ! test -f "/pds/pds.env"; then
@@ -33,6 +52,8 @@ if test -f "/pds/pds.env"; then
   set -a
   . /pds/pds.env
   set +a
-       
+  
+  [ -n "$COMMAND" ] && sleep 20
+
   $COMMAND node --enable-source-maps /app/index.js
 fi
